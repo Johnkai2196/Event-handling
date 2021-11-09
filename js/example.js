24904d414a97800d082c3565bf168c95acf229ae
Johnkai2196 <johnkaic@metropolia.fi>
2021-11-09 15:42:46 +0200

    example

---
+++ js/example.js
@@ -0,0 +1,12 @@
+'use strict';
+const link = document.querySelector('#link');
+link.addEventListener('click', (evt) => {
+  alert('link has been click');
+  evt.preventDefault();
+});
+
+const sayHello = () => {
+  alert('Hello');
+};
+
+link.addEventListener('mouseover', sayHello);
