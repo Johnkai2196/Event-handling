6b075f9dc7c57fe74a10bf0e221c394bef30c123
Johnkai Cortez <73104201+Johnkai2196@users.noreply.github.com>
2021-11-09 23:16:57 +0200

    it pop

--- js/example.js
+++ js/example.js
@@ -1,5 +1,7 @@
 'use strict';
 const link = document.querySelector('#link');
+let id = document.getElementById('pop');
+let size;
 link.addEventListener('click', (evt) => {
   alert('link has been click');
   evt.preventDefault();
@@ -10,3 +12,21 @@ const sayHello = () => {
 };
 
 link.addEventListener('mouseover', sayHello);
+
+window.addEventListener('keydown', () => {
+  if (!(parseInt(size) == 100)) {
+    size = document.getElementById('pop').style.fontSize;
+    if (event.key == 'ArrowUp') {
+
+      let test = parseInt(size) + 10;
+      document.getElementById('pop').style.fontSize = test + 'px';
+    }
+    if (event.key == 'ArrowDown') {
+      let test = parseInt(size) - 10;
+      document.getElementById('pop').style.fontSize = test + 'px';
+    }
+  } else {
+    id.textContent = '💥';
+    event.preventDefault();
+  }
+});
