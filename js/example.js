ab04bb3e4665145cf71f98b6f886c0ce70423e80
Johnkai Cortez <73104201+Johnkai2196@users.noreply.github.com>
2021-11-11 18:09:53 +0200

    mouse trail

--- js/example.js
+++ js/example.js
@@ -30,3 +30,20 @@ window.addEventListener('keydown', () => {
     event.preventDefault();
   }
 });
+let zero = 0;
+let div = [];
+let i = 0;
+
+for (i; i < 1000; i++) {
+  let dot = document.createElement('div');
+  dot.className = 'trail';
+  document.body.appendChild(dot);
+  div.push(dot);
+}
+
+window.addEventListener('mousemove', event => {
+  let dot = div[zero];
+  dot.style.left = (event.pageX - 4) + 'px';
+  dot.style.top = (event.pageY - 4) + 'px';
+  zero = (zero + 1) % div.length;
+});
