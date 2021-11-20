f603a3b59db3cef6d964af71b73dd9d2a7424c40
Johnkai2196 <johnkaic@metropolia.fi>
2021-11-20 18:47:53 +0200

    tab code

--- js/tabs.js
+++ js/tabs.js
@@ -1,14 +1,51 @@
 'use strict';
-let activeTab;
+let current;
 
 let currentBody;
 
-function switchTab(event) {
+const switchTab = (event) => {
 
-}
+  current.classList.remove('selected');
+  current = event.target;
+  current.classList.add('selected');
 
-function asTabs(node) {
+  let tabContent = document.querySelectorAll('div');
+  currentBody.classList.toggle('hidden');
 
-}
+  switch (current.textContent) {
+    case 'one':
+      currentBody = tabContent[0];
+      break;
+    case 'two':
+      currentBody = tabContent[1];
+      break;
+    case 'three':
+      currentBody = tabContent[2];
+      break;
+  }
 
-asTabs(document.querySelector('tab-panel'));
+  currentBody.classList.toggle('hidden');
+};
+
+const tabs = (node) => {
+  let tabLength = node.children.length;
+
+  for (let i = 0; i < tabLength; i++) {
+    let tabButton = document.createElement('button');
+
+    tabButton.textContent = document.querySelectorAll('div')[i].getAttribute(
+        'data-tabname');
+    node.querySelectorAll('div')[i].classList.add('hidden');
+    tabButton.addEventListener('click', switchTab);
+    node.insertBefore(tabButton, document.querySelector('div'));
+  }
+
+  current = document.querySelector('button');
+  current.classList.add('selected');
+
+  currentBody = document.querySelector('div');
+  currentBody.classList.toggle('hidden');
+
+};
+
+tabs(document.querySelector('tab-panel'));
